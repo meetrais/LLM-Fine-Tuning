98439c9d98176aa05bcde465a68addd2efe3497e
meetrais <meetrais@gmail.com>
2023-11-09 11:01:03 -0600

    More quantization.

--- finetune_mistral_7b.py
+++ finetune_mistral_7b.py
@@ -1,10 +1,10 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
-import bitsandbytes
+from transformers import BitsAndBytesConfig
 import torch
 
 model_id = "mistralai/Mistral-7B-v0.1"
 
-model = AutoModelForCausalLM.from_pretrained(model_id, load_in_4bit=True, device_map="auto", bnb_4bit_compute_dtype=torch.float16 )
+model = AutoModelForCausalLM.from_pretrained(model_id, load_in_4bit=True, device_map="auto")
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 
 #print(model)
@@ -14,4 +14,24 @@ device = "cuda:0"
 
 inputs = tokenizer(text, return_tensors="pt").to(device)
 outputs = model.generate(**inputs, max_new_tokens=20)
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+
+quantization_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_compute_dtype=torch.bfloat16
+)
+
+model_cd_bf16 = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=quantization_config)
+
+outputs = model_cd_bf16.generate(**inputs, max_new_tokens=20)
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+
+nf4_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+)
+
+model_nf4 = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=nf4_config)
+
+outputs = model_nf4.generate(**inputs, max_new_tokens=20)
 print(tokenizer.decode(outputs[0], skip_special_tokens=True))
