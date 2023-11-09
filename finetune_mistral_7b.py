84a4c30e0c0fccffd0f45be41a28e9cfec65a91e
meetrais <meetrais@gmail.com>
2023-11-09 11:26:33 -0600

    Final Quantized model.

--- finetune_mistral_7b.py
+++ finetune_mistral_7b.py
@@ -3,16 +3,17 @@ from transformers import BitsAndBytesConfig
 import torch
 
 model_id = "mistralai/Mistral-7B-v0.1"
-
-model = AutoModelForCausalLM.from_pretrained(model_id, load_in_4bit=True, device_map="auto")
 tokenizer = AutoTokenizer.from_pretrained(model_id)
-
-#print(model)
-
 text = "Capital of USA is"
 device = "cuda:0"
 
 inputs = tokenizer(text, return_tensors="pt").to(device)
+"""
+model = AutoModelForCausalLM.from_pretrained(model_id, load_in_4bit=True, device_map="auto")
+
+#print(model)
+
+
 outputs = model.generate(**inputs, max_new_tokens=20)
 print(tokenizer.decode(outputs[0], skip_special_tokens=True))
 
@@ -34,4 +35,26 @@ nf4_config = BitsAndBytesConfig(
 model_nf4 = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=nf4_config)
 
 outputs = model_nf4.generate(**inputs, max_new_tokens=20)
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+
+double_quant_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+)
+
+model_double_quant = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=double_quant_config)
+
+outputs = model_double_quant.generate(**inputs, max_new_tokens=20)
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+"""
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16
+)
+
+model_4bit = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=bnb_config)
+
+outputs = model_4bit.generate(**inputs, max_new_tokens=20)
 print(tokenizer.decode(outputs[0], skip_special_tokens=True))
