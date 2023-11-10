11c586c493f5fb8dc7a9282f94dd09222a3257b3
meetrais <meetrais@gmail.com>
2023-11-10 16:04:13 -0600

    Changes in call to fine-tuned mistral model.

--- call_finetune_mistral_7b.py
+++ call_finetune_mistral_7b.py
@@ -1,10 +1,17 @@
 import torch
 from peft import PeftModel, PeftConfig
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import BitsAndBytesConfig
 
 peft_model_id = "meetrais/finetuned_mistral_7b"
 config = PeftConfig.from_pretrained(peft_model_id)
-model = AutoModelForCausalLM.from_pretrained(config.base_model_name_or_path, return_dict=True, load_in_4bit=True, device_map='auto')
+bnb_config = BitsAndBytesConfig(
+load_in_4bit=True,
+bnb_4bit_use_double_quant=True,
+bnb_4bit_quant_type="nf4",
+bnb_4bit_compute_dtype=torch.bfloat16
+)
+model = AutoModelForCausalLM.from_pretrained(peft_model_id,  quantization_config=bnb_config, device_map='auto')
 tokenizer = AutoTokenizer.from_pretrained(config.base_model_name_or_path)
 
 if tokenizer.pad_token is None:
@@ -13,8 +20,6 @@ text = "Capital of USA is"
 device = "cuda:0"
 
 inputs = tokenizer(text, return_tensors="pt").to(device)
-# Load the Lora model
-model = PeftModel.from_pretrained(model, peft_model_id, bnb_4bit_compute_type=torch.float16 )
 
 outputs = model.generate(**inputs, max_new_tokens=20)
 print(tokenizer.decode(outputs[0], skip_special_tokens=True))
