07e73bf5a8ca7ddce47980410b4df62e8166513e
Rais Kazi <meetrais@gmail.com>
2024-07-23 23:10:17 -0500

    Finetuned Meta Llama 3.1 8B Instruct model.

---
+++ finetune_llama_3_1_8B-Instruct.py
@@ -0,0 +1,45 @@
+import transformers  as transformers
+from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments
+from trl import SFTTrainer
+from transformers import BitsAndBytesConfig
+import torch
+import torch.nn as nn
+from peft import LoraConfig, get_peft_model 
+from datasets import load_dataset
+
+model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+if tokenizer.pad_token is None:
+    tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+text = "Capital of USA is"
+device = "cuda:0"
+
+inputs = tokenizer(text, return_tensors="pt").to(device)
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16,
+    load_in_8bit=False
+)
+
+model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=bnb_config)
+
+config  = LoraConfig(
+    r=16,
+    lora_alpha=32,
+    lora_dropout=0.05,
+    bias = 'none',
+    task_type="CAUSAL_LM",
+    target_modules=["q_proj","k_proj","v_proj","o_proj","gate_proj","up_proj","down_proj","lm_head"]
+)
+
+model = get_peft_model(model, config)
+outputs = model.generate(**inputs, max_new_tokens=30)
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+
+model.push_to_hub("meetrais/finetuned_Meta-Llama-3.1-8B-Instruct",
+                  token="Your_HF_Token",
+                  commit_message="basic training",
+                  private=True)
